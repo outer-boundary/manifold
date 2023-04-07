f6ae99e3aa7912af67370026c2ef08ba177752f4
tri/KUMA <trikuma@hotmail.com>
2023-04-07 11:20:08 +0800

    Read planetscale connection string from env var

--- manifold-backend/src/main.rs
+++ manifold-backend/src/main.rs
@@ -1,9 +1,17 @@
 use actix_web::{App, HttpServer};
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(App::new)
+    dotenv::from_filename(".env").ok();
+
+    let url = env::var("DATABASE_URL").expect("DATABASE_URL env var could not be found");
+    println!("{url}");
+
+    HttpServer::new(|| App::new())
         .bind(("127.0.0.1", 8080))?
         .run()
-        .await
+        .await?;
+
+    Ok(())
 }
