fa55c4bbb6a53f78eac6228e285453a2979dc7d3
tri/KUMA <trikuma@hotmail.com>
2023-04-10 11:04:28 +0800

    Create connection pool

--- manifold-backend/src/main.rs
+++ manifold-backend/src/main.rs
@@ -1,12 +1,16 @@
 use actix_web::{App, HttpServer};
+use sqlx::mysql;
 use std::env;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    dotenv::from_filename(".env").ok();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::from_filename(".env")?;
 
-    let url = env::var("DATABASE_URL").expect("DATABASE_URL env var could not be found");
-    println!("{url}");
+    let database_url = env::var("DATABASE_URL")?;
+    let pool = mysql::MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
 
     HttpServer::new(|| App::new())
         .bind(("127.0.0.1", 8080))?
