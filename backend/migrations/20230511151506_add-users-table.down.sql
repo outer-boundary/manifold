66f6c2b48660938d1d61b0b76cca1c590c540397
tri/KUMA <trikuma@hotmail.com>
2023-05-21 21:56:08 +0800

    Remove login identity tables from migration files

--- backend/migrations/20230511151506_add-users-table.down.sql
+++ backend/migrations/20230511151506_add-users-table.down.sql
@@ -1,7 +1,4 @@
--- Drop created tables. 
-DROP TABLE li_phone_number;
-DROP TABLE li_email_password;
-DROP TABLE login_identity;
+-- Drop created tables.
 DROP TABLE users;
 
 -- Drop created functions.
--- backend/migrations/20230511151506_add-users-table.up.sql
+++ backend/migrations/20230511151506_add-users-table.up.sql
@@ -25,37 +25,3 @@ CREATE TABLE users (
   created_at datetime NOT NULL DEFAULT current_timestamp,
   updated_at datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
 );
-
--- Generic table used for mapping a user to their login identities. This allows the user to use multiple different
--- identities to authenticate with a single account.
-CREATE TABLE login_identity (
-  id binary(16) PRIMARY KEY NOT NULL DEFAULT (uuid_to_bin(uuid(), true)),
-  user_id binary(16) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-
-  identity_type enum("email_password", "phone_number")
-);
-
--- Login identity table for email and password authentication.
-CREATE TABLE li_email_password (
-  id binary(16) NOT NULL,
-  FOREIGN KEY (id) REFERENCES login_identity(id),
-
-  email varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
-  password_hash varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  salt varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-
-  created_at datetime NOT NULL DEFAULT current_timestamp,
-  updated_at datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
-);
-
--- Login identity table for phone number authentication.
-CREATE TABLE li_phone_number(
-  id binary(16) NOT NULL,
-  FOREIGN KEY (id) REFERENCES login_identity(id),
-
-  phone_number varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
-
-  created_at datetime NOT NULL DEFAULT current_timestamp,
-  updated_at datetime NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
-);
