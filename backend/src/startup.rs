d3d2c0bc92ab8f439fd46d05e4917c148d2203c3
tri/KUMA <trikuma@hotmail.com>
2023-07-02 16:53:17 +0800

    Add session storage middleware

--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -1,8 +1,10 @@
 use crate::{
     routes::{health_check::health_check_route, users::users_scope},
-    util::configuration::{Configuration, DatabaseConfiguration},
+    util::configuration::{Configuration, DatabaseConfiguration, Environment},
 };
+use actix_session::{storage::CookieSessionStore, SessionMiddleware};
 use actix_web::{
+    cookie,
     dev::Server,
     web::{scope, Data},
     App, HttpServer,
@@ -49,11 +51,22 @@ pub async fn get_connection_pool(settings: &DatabaseConfiguration) -> MySqlPool 
 }
 
 async fn run(listener: TcpListener, db_pool: MySqlPool, config: Configuration) -> Result<Server> {
-    let cfg = deadpool_redis::Config::from_url(config.redis.url);
-    let redis_pool = cfg.create_pool(Some(deadpool_redis::Runtime::Tokio1))?;
+    let redis_config = deadpool_redis::Config::from_url(config.redis.url);
+    let redis_pool = redis_config.create_pool(Some(deadpool_redis::Runtime::Tokio1))?;
+
+    let secret_key = cookie::Key::from(config.secret.hmac_secret.as_bytes());
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(if let Environment::Development = config.environment {
+                SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+                    .cookie_http_only(true)
+                    .cookie_same_site(cookie::SameSite::None)
+                    .cookie_secure(true)
+                    .build()
+            } else {
+                SessionMiddleware::new(CookieSessionStore::default(), secret_key.clone())
+            })
             .app_data(Data::new(db_pool.clone()))
             .app_data(Data::new(redis_pool.clone()))
             .service(
