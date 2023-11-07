9ce79bcc748f880f5d18f4f89000860a14fdab83
tri/KUMA <trikuma@hotmail.com>
2023-11-07 12:03:48 +0800

    wip adding kraos to frontend

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import MainSection from "./main-section.svelte";
 	import Overlay from "./overlay.svelte";
 	import { afterUpdate, beforeUpdate } from "svelte";
+	import { redirect } from "@sveltejs/kit";
+  import {Configuration, FrontendApi} from '@ory/client';
 
 	let showSidebar = true;
 	beforeUpdate(() => {
@@ -14,6 +16,23 @@
 	afterUpdate(() => {
 		document.getElementById("mainSection")!.style.borderRadius = showSidebar ? "" : "0px";
 	});
+
+  const basePath = process.env.REACT_APP_ORY_URL || "http://localhost:4000";
+  const ory = new FrontendApi(
+    new Configuration({
+      basePath,
+      baseOptions: {
+        withCredentials: true,
+      },
+    }),
+  );
+
+  // const authenticated = cookies.get("id");
+
+  // If an unauthenticated user tries to go to any page besides the below, redirect them to the login page
+  // if (!authenticated && !["/login", "/signup", "/verify"].some(x => x === url.pathname)) {
+  //   throw redirect(307, "/login");
+  // }
 </script>
 
 <div id="main">
